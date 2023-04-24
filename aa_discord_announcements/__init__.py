adcdf4de1861dc26f779bdcde8aa1f05c4f09987
Peter Pfeufer <info@ppfeufer.de>
2023-04-24 12:45:12 +0200

    [FIX] Package name for metadata

--- aa_discord_announcements/__init__.py
+++ aa_discord_announcements/__init__.py
@@ -5,7 +5,7 @@ A couple of variables to use throughout the app
 # Standard Library
 from importlib import metadata
 
-__version__ = metadata.version("aa-bulletin-board")
+__version__ = metadata.version("aa-discord-announcements")
 __title__ = "Discord Announcements"
 
 del metadata
