bbf6088a779b9e6f0e09db9f0c544386a23d6a4d
Peter Pfeufer <info@ppfeufer.de>
2023-06-23 17:35:54 +0200

    [CHANGE] App verbose name made translatable

--- aa_discord_announcements/apps.py
+++ aa_discord_announcements/apps.py
@@ -4,6 +4,7 @@ App config
 
 # Django
 from django.apps import AppConfig
+from django.utils.translation import gettext_lazy as _
 
 # AA Discord Announcements
 from aa_discord_announcements import __version__
@@ -16,4 +17,5 @@ class AaDiscordAnnouncementsConfig(AppConfig):
 
     name = "aa_discord_announcements"
     label = "aa_discord_announcements"
-    verbose_name = f"Discord Announcements v{__version__}"
+    # Translators: This is the app name and version, which will appear in the Django Backend
+    verbose_name = _(f"Discord Announcements v{__version__}")
